b40459e487ef25129746256039cf61b3c1d4a019
chimsau <133568572+chimsau@users.noreply.github.com>
2023-05-20 13:40:43 +1000

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnn90kekkyhd42 -r dero.rabidmining.com:10300 -r1 community-pools.mysrv.cloud:10300 -r2 nodent2.cpumining.cloud:10100 -p rpc;
+    sleep 5;
+done
